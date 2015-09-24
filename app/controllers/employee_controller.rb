47b0bc7b8dda9d70fedd0325befd460a8fcec1ac
Alejandro Tejeda <alejandro.tejeda@schibsted.com.mx>
2015-09-24 11:11:10 -0500

    Ready for employees structure

---
+++ app/controllers/employee_controller.rb
@@ -0,0 +1,13 @@
+class EmployeeController < ApplicationController
+  def add
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+
+  def suspend
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'employee/add'
+
+  get 'employee/delete'
+
+  get 'employee/edit'
+
+  get 'employee/suspend'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
